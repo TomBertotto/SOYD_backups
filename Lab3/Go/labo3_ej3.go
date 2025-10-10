862c2f8f4a7553cbf1b0b678f12cca0e9aba79f1
Tom <tom.bertotto@gmail.com>
2025-10-10 17:52:19 -0300

    backup nuevo, punto 3

--- Lab3/Go/labo3_ej3.go
+++ Lab3/Go/labo3_ej3.go
@@ -7,13 +7,14 @@ import (
 	"strings"
 	"sync"
 	"os"
+	"strconv"
 )
 
 const (
 	servidor_URL = "https://desarrollo.cs.uns.edu.ar"
 	formato_archivo = "labo2_ia_vector_"
-	//pag_principal_url = servidor_URL + "/~user_00/index-2025.php"
-
+	//pag_principal_url = servidor_URL + "/~user_00/index-2025.php"	
+	tamanio_arreglos = 5
 )
 
 /*
@@ -76,28 +77,75 @@ func analizarArchivo(usuario, contenido string, palabras []string) {
 		fmt.Println("No coinciden la cantidad de palabras con cantidad de valores\n")
 		os.Exit(1)
 	}
+	
 	for i:= 0; i < len(palabras_archivo); i++ {
 		palabras_archivo[i] = strings.TrimSpace(palabras_archivo[i])
+		palabras_archivo[i] = strings.ToLower(palabras_archivo[i])
 		valores_archivo[i] = strings.TrimSpace(valores_archivo[i])
+		valores_archivo[i] = strings.ToLower(valores_archivo[i])
+		for j:= 0; j < len(palabras); j++ {
+			conversion, err := strconv.Atoi(valores_archivo[i])
+			if err == nil {
+				if palabras_archivo[i] == palabras [j] {
+					fmt.Printf("Palabra '%s' con peso: %d\n", palabras_archivo[i], conversion)
+				}
+			}
+		}
 	}
-
 	fmt.Printf("Palabras %s \nValores: %s\n", palabras_archivo, valores_archivo)
+
 }
+
+
+
 
 func main() {
 	
 	if len(os.Args) < 4 {
 		fmt.Println("Error en cantidad de argumentos")
 		fmt.Println("Formato: ./labo3_ej3 <palabra1> <palabra2> <palabra3>")
+		fmt.Println("")	
+		fmt.Println("Palabras positivas: bueno, genial, positivo, feliz, contento")
+		fmt.Println("Palabras negativas: peor, triste, odio, terrible, negativo")
+		fmt.Println("Palabras testeo: probar, prueba, test, intento, funciona")
 		os.Exit(1)
 	}
+	
+	p_positivas := [tamanio_arreglos]string{"bueno", "correcto", "positivo", "feliz", "contento"}
+	p_negativas := [tamanio_arreglos]string{"peor", "triste", "odio", "terrible", "falla"}
+	p_testeo := [tamanio_arreglos]string{"prueba", "correcto", "test", "falla", "funciona"}
 
 	var palabras_analizar []string
 	palabras_analizar = make([]string, len(os.Args) - 1)
+	var palabras_coinciden []string
+	palabras_coinciden = make([]string, len(os.Args) - 1)
+	
+	contP := 0
+	contN := 0
+	contT := 0
+
 	for i := 0; i  < len(palabras_analizar); i++ {
 		palabras_analizar[i] = os.Args[i + 1]
+		for j := 0; j < tamanio_arreglos; j++ {
+			if palabras_analizar[i] == p_positivas [j] {contP = 1}	
+			if palabras_analizar[i] == p_negativas [j] {contN = 1}
+			if palabras_analizar[i] == p_testeo [j] {contT = 1}
+		}
+		fmt.Printf("Palabra '%s' con categoría: ", palabras_analizar[i])
+		if contT != 0 { fmt.Printf("testeo ")}
+		if contP != 0 { fmt.Printf("positivo")}
+		if contN != 0 { fmt.Printf("negativo")}
+		if contT == 0 && contP == 0 && contN == 0 {
+			fmt.Printf("[NO ENCONTRADA EN LISTA]")
+		} else {
+			palabras_coinciden = append(palabras_coinciden, palabras_analizar[i])	
+		}
+		contP = 0
+		contN = 0
+		contT = 0
+		fmt.Println("\n")
 	}
-
+	fmt.Println("======================================\n")
 
 	cant_alumnos := 1
 	var lu_alumnos[1]string
@@ -105,8 +153,9 @@ func main() {
 	var wg sync.WaitGroup
 	for i := 0; i < cant_alumnos ; i++ {
 		wg.Add(1)
-		go descargar_analizar(lu_alumnos[i], &wg, palabras_analizar)
+		go descargar_analizar(lu_alumnos[i], &wg, palabras_coinciden)
 	}
 	wg.Wait()
 	
-}	
+}
+
