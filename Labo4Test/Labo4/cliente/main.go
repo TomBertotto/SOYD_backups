d83903da251382392b459d04c863fb31d76c70e7
Tom <tom.bertotto@gmail.com>
2025-11-19 16:54:01 -0300

    mejoras cliente y namenode backup

--- Labo4Test/Labo4/cliente/main.go
+++ Labo4Test/Labo4/cliente/main.go
@@ -5,8 +5,96 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"strings"
 	"os"
 )
+
+
+const BLOCK_TAMANIO = 1024
+
+func ejecutarPut(nombre_archivo string, addrNamenode string) {
+	archivo, err := os.ReadFile(nombre_archivo)
+	if err != nil {
+		fmt.Println("Error abriendo archivo:", err)
+		return
+	}
+
+	tamanio_archivo := len(archivo)
+	cant_bloques := tamanio_archivo / BLOCK_TAMANIO
+	if tamanio_archivo % BLOCK_TAMANIO != 0 {
+		cant_bloques++
+	}
+	
+	fmt.Printf("Archivo: %s (%d bytes) -> %d bloques de 1KB\n", nombre_archivo, tamanio_archivo, cant_bloques)
+
+	msg := fmt.Sprintf("put %s %d\n"), nombre_archivo, cant_bloques)
+
+	conn, err := net.Dial("tcp", addrNamenode)
+	if err != nil {
+		fmt.Println("Error conectado con el namenode:", err)
+		return
+	}
+	defer conn.Close()
+
+	conn.Write([]byte(msg))
+	
+	fmt.Println("Cliente esperando asignacion de datanodes...")
+
+	reader := bufio.NewReader(conn)
+	for {
+		linea, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error al recibir del namenode")
+			break
+		}
+		linea = strings.TrimSpace(linea)
+
+		if linea == "END" { //establezco como protocolo que el namenode va a usar END al final
+			break
+		}
+
+		fmt.Println("---", linea, "---")
+	}
+
+	//FALTA IMPLEMENTAR ENVIO A LOS DATANODES!!!!!!!!!!!!!!!!!!!!!!!
+
+
+}
+
+
+
+func procesarComando(input string, addrNamenode string) {
+	partes := strings.Fields(input)
+	comando := strings.ToLower(parts[0])
+
+	switch comando {
+	case "put": 
+		if len(partes) < 2 { //ver si se puede refactorizar para no tener tanto codigo de conexion duplicado
+			fmt.Println("Incorrecto, uso: put <archivo>")
+			return
+		}
+
+		ejecutarPut(partes[1], addrNamenode)
+	
+	case "get":
+		if len(partes) < 2 {
+			fmt.Println("Incorrecto, uso: get <archivo>")
+			return
+		}
+		ejecutarGet(partes[1], addrNamenode)	
+	
+	case "ls":
+		ejecutarLsInfo(partes[0], addrNamenode)
+	case "info":
+		if len(partes) < 2 {
+			fmt.Println("Incorrecto, uso: info <archivo>")
+			return
+		}
+		ejecutarLsInfo(partes[1], addrNamenode)
+		
+	default: fmt.Println("Comando no válido:", comando)
+	}
+}
 
 func main() {
 	if len(os.Args) < 2 {
@@ -16,21 +104,31 @@ func main() {
 	}
 
 	addrNamenode := os.Args[1]
-	
-	conn, err := net.Dial("tcp", addrNamenode)
 
-	if err != nil {
-		log.Fatal("No se pudo conectar al namenode:", err)
+	fmt.Println("----------CLIENTE DFS----------")
+	fmt.Println("Comandos: put <archivo>, get<archivo>, ls, info <archivo>, exit")
+
+	for {
+		fmt.Prtint("dfs> ")
+
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error leyendo la entrada:", err)
+			continue
+		}
+
+		input = strings.TrimSpace(input)
+		if input == "" {
+			continue
+		}
+
+		if input == "exit" {
+			fmt.Println("Saliendo del cliente...")
+			return
+		}
+
+		procesarComando(input, addrNamenode)
 	}
-	defer conn.Close()
-
-	fmt.Println("Conectando al Namenode:", addrNamenode)
-	conn.Write([]byte("Hola desde el cliente!\n"))
-
-	respuesta, err := bufio.NewReader(conn).ReadString("\n")
-	if err != nil {
-		log.Fatal("Error leyendo respuesta:", err)
-	}
-
-	fmt.Println("Respuesta del namenode:", respuesta)
 }
+
+
--- Labo4Test/Labo4/namenode/main.go
+++ Labo4Test/Labo4/namenode/main.go
@@ -5,20 +5,79 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"strings"
+	"strconv"
 )
 
-func administrarConexion(conn net.Conn) {
-	defer conn.Close()
-	reader := bufio.NewReader(conn)
+var datanodes = []string {//hardcodeados
+	"192.168.0.10:5000",
+	"192.168.0.11:5001",
+}
 
-	msg, err := reader.ReadString("\n")
+func ejecutarPut(partes []string, conn net.Conn) {
+	nombre_archivo := partes[1]
+	cant_bloques, err := strconv.Atoi(partes[2])
 	if err != nil {
-		log.Println("Error leyendo", err)
+		conn.Write([]byte("NAMENODE ERROR: cantidad de bloques no valida\n"))
 		return
 	}
 
-	fmt.Println("Namenode recibió:", msg)
-	conn.Write([]byte("Mensaje recibido por el namenode!\n")
+	fmt.Printf("NAMENODE: Recibi PUT %s con %d bloques\n", nombre_archivo, cant_bloques)
+
+	for i:= 0; i < cant_bloques; i++ {
+		dn := datanodes[i % len(datanodes)] //tipo RoundRobin
+		linea := fmt.Sprintf("b%d %s\n", i, dn)
+		conn.Write([]byte(linea))
+	}
+
+	conn.Write([]byte("END\n")) //por protocolo para que reciba el cliente
+}
+
+
+func ejecutarGet(partes []string, conn net.Conn) {
+	nombre_archivo := partes[1]
+	fmt.Printf("NAMENODE: Recibi GET de %s\n", nombre_archivo)
+	//FALTA IMPLEMENTAR
+
+}
+
+func ejecutarLS(conn net.Conn) {
+//FALTA IMPLEMENTAR
+}
+
+func ejecutarInfo(partes []string, conn net.Conn) {
+//FALTA IMPLEMENTAR
+}
+func administrarConexion(conn net.Conn) {
+	defer conn.Close() //ver si conviene cerrarla o esperar a que el cliente termine para enviar(que envie el ACK)
+	reader := bufio.NewReader(conn)
+
+	linea, err := reader.ReadString('\n')
+	if err != nil {
+		log.Println("NAMENODE: error leyendo el envio de CLIENT", err)
+		return
+	}
+	
+	linea = strings.TrimSpace(linea)
+	partes := strings.Fields(linea)
+	//el cliente YA verifica que los comandos se ingresaron bien
+
+	comando := strings.ToLower(partes[0])
+
+	switch comando {
+	case "put":
+		ejecutarPut(partes, conn)
+	case "get":
+		ejecutarGet(partes, conn)
+	case "ls":
+		ejecutarLS(conn)
+	case "info":
+		ejecutarInfo(partes, conn)
+	default: conn.Write([]byte("Comando no valido\n"))//a la defensiva, no deberia pasar este caso
+	}
+
+	//fmt.Println("Namenode recibió:", msg)
+	//conn.Write([]byte("Mensaje recibido por el namenode!\n"))
 }
 
 func main() {
